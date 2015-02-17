b251f10d622564b8938f9405d6fbd3b177d3fd0f
tehotdel <tehotdel@eltc.ru>
2015-02-17 17:03:17 +0700

    client service

--- environments/production.rb
+++ environments/production.rb
@@ -1,3 +1,19 @@
 name "production"
 description "For production!"
-cookbook "apache", "= 0.2.0"
+#cookbook "apache", "= 0.2.0"
+#cookbook "chef-client", "= 4.2.0"
+#cookbook "cron", "= 1.6.1"
+#cookbook "logrotate", "= 1.8.0"
+
+cookbook_versions({
+    "apache" => "= 0.2.0",
+    "chef-client" => "= 4.2.0",
+    "logrotate" => "= 1.8.0",
+    "cron" => "1.6.1"
+  })
+
+default_attributes(
+  "chef_client" => {
+    "interval" => 300
+  }
+)
